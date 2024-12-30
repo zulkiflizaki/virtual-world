ad78979461858ce54e358e4157bb4437192f07c8
Zulkifli Zaki <zulkiflizaki@hotmail.com>
2024-12-30 11:38:29 +0800

    Build workable Graph Editor

---
+++ js/graphEditor.js
@@ -0,0 +1,80 @@
+class GraphEditor {
+  constructor(canvas, graph) {
+    this.canvas = canvas;
+    this.graph = graph;
+
+    this.ctx = this.canvas.getContext("2d");
+
+    this.selected = null;
+    this.hovered = null;
+    this.dragging = false;
+    this.mouse = null;
+
+    this.#addEventListeners();
+  }
+
+  #addEventListeners() {
+    this.canvas.addEventListener("mousedown", this.#handleMouseDown.bind(this));
+    this.canvas.addEventListener("mousemove", this.#handleMouseMove.bind(this));
+    this.canvas.addEventListener("contextmenu", (evt) => evt.preventDefault());
+    this.canvas.addEventListener("mouseup", () => (this.dragging = false));
+  }
+
+  #handleMouseDown(evt) {
+    if (evt.button == 2) {
+      // right click
+      if (this.selected) {
+        this.selected = null;
+      } else if (this.hovered) {
+        this.#removePoint(this.hovered);
+      }
+    }
+    if (evt.button == 0) {
+      // left click
+      if (this.hovered) {
+        this.#selectPoint(this.hovered);
+        this.dragging = true;
+        return;
+      }
+      this.graph.addPoint(this.mouse);
+      this.#selectPoint(this.mouse);
+      this.hovered = this.mouse;
+    }
+  }
+
+  #handleMouseMove(evt) {
+    this.mouse = new Point(evt.offsetX, evt.offsetY);
+    this.hovered = getNearestPoint(this.mouse, this.graph.points, 12);
+    if (this.dragging == true) {
+      this.selected.x = this.mouse.x;
+      this.selected.y = this.mouse.y;
+    }
+  }
+
+  #selectPoint(point) {
+    if (this.selected) {
+      this.graph.tryAddSegment(new Segment(this.selected, point));
+    }
+    this.selected = point;
+  }
+
+  #removePoint(point) {
+    this.graph.removePoint(point);
+    this.hovered = null;
+    if (this.selected == point) {
+      this.selected = null;
+    }
+  }
+
+  display() {
+    this.graph.draw(this.ctx);
+    if (this.selected) {
+      const intent = this.hovered ? this.hovered : this.mouse;
+      new Segment(this.selected, intent).draw(ctx, { dash: [3, 3] });
+      this.selected.draw(this.ctx, { outline: true });
+    }
+    if (this.hovered) {
+      this.hovered.draw(this.ctx, { fill: true });
+    }
+  }
+}
